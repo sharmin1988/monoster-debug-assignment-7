5092a56922522d09ffda7c6139330b9f4db867f6
sharmin1988 <rasnasharmin11@gmail.com>
2022-09-14 22:53:08 +0600

    fix seconds(time) display problem

--- script.js
+++ script.js
@@ -136,7 +136,8 @@ displayHistory();
 setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
+  
 
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? Math.round(timeSpent) : 0} seconds`;
 }, 1000);
